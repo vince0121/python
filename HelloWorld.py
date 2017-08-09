0e03f71b7dc1babb8a72d40a4245860bad64ca4c
vince0121 <vince0121@users.noreply.github.com>
2017-08-09 10:02:37 +0800

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print("hello world")
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+age = 18
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+a = 123
+print(a)
+a = 'ABC'
+print(a)
+b=a
+a='XYZ'
+print(b)
